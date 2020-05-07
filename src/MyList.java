79a3f68505c7c13b7e9d645ea3aecd2f3001bc49
VuPham93 <62596412+VuPham93@users.noreply.github.com>
2020-05-07 14:21:07 +0700

    Initial commit

---
+++ src/MyList.java
@@ -0,0 +1,90 @@
+import java.util.Arrays;
+
+public class MyList<E> {
+    private int size = 0;
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object elements[];
+
+    public MyList() {
+        elements = new Object[DEFAULT_CAPACITY];
+    }
+
+    public MyList(int capacity) {
+        elements = new Object[capacity];
+    }
+
+    public void add(int index, E element) {
+        if (index > size || index < 0)
+            throw new IndexOutOfBoundsException("Index: "+index+", Size: "+size);
+        ensureCapacity(1);
+        for (int i = elements.length - 1; i > index; i--) {
+                elements[i] = elements[i - 1];
+        }
+        size++;
+        elements[index] = element;
+    }
+
+    public E remove(int index) {
+        while (index < elements.length - 1) {
+            elements[index] = elements[index + 1];
+            index++;
+        }
+        elements[size] = null;
+        size--;
+        return null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public MyList<E> clone() {
+        MyList<E> clone = new MyList<E>(size);
+        clone.size = size;
+        if (size >= 0) System.arraycopy(elements, 0, clone.elements, 0, size);
+        return clone;
+    }
+
+    public boolean contains(Object findElement) {
+        for (Object o : elements) {
+            if (findElement.equals(o)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int indexOf (E o) {
+        for (int i = 0; i < elements.length; i++) {
+            if (o.equals(elements[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public boolean add(E e) {
+        if (size == elements.length) {
+            ensureCapacity(1);
+        }
+        elements[size] = e;
+        size++;
+        return true;
+    }
+
+    public void ensureCapacity(int minCapacity) {
+        int newSize = elements.length + minCapacity;
+        elements = Arrays.copyOf(elements, newSize);
+    }
+
+    public E get(int i) {
+        return (E) elements[i];
+    }
+
+    public void clear() {
+        for (int i = 0 ; i < elements.length; i++) {
+            elements[i] = null;
+        }
+        size = 0;
+    }
+}
---
+++ src/MyListTest.java
@@ -0,0 +1,68 @@
+public class MyListTest {
+    public static void main(String[] args) {
+        MyList<String> stringMyList = new MyList<String>();
+        stringMyList.add("a");
+        stringMyList.add("b");
+        stringMyList.add("c");
+        stringMyList.add("e");
+        stringMyList.add("f");
+
+        System.out.print("\n" + "Array list 1: " );
+        for (int i = 0; i < stringMyList.size(); i++) {
+            System.out.print("\t" + stringMyList.get(i));
+        }
+
+        stringMyList.add("h");
+        stringMyList.add(3, "d");
+        stringMyList.add("i");
+        stringMyList.add(8, "j");
+        stringMyList.add(6, "g");
+        stringMyList.add(10, "k");
+
+        System.out.print("\n" + "After add more characters");
+        System.out.print("\n" + "Array list 1: " );
+        for (int i = 0; i < stringMyList.size(); i++) {
+            System.out.print("\t" + stringMyList.get(i));
+        }
+
+        System.out.print("\n" + "Clone of array list 1: " );
+        MyList<String> clone = stringMyList.clone();
+        for (int i = 0; i < clone.size(); i++) {
+            System.out.print("\t" + clone.get(i));
+        }
+
+        MyList<Object> myList = new MyList<Object>(5);
+        myList.add("String");
+        myList.add(true);
+        myList.add(1);
+        myList.add(2.0);
+
+        System.out.println();
+        System.out.print("\n" + "Array list 2: " );
+        for (int i = 0; i < myList.size(); i++) {
+            System.out.print("\t" + myList.get(i));
+        }
+        System.out.println("\n" + "Array list size is: " + myList.size());
+
+        double findValue = 2.0;
+        System.out.print("\n" + "Find value (" + findValue + ") is in the arraylist? " + myList.contains(findValue));
+        System.out.print("\n" + "Index of find value = " + myList.indexOf(findValue));
+
+        int index = 2;
+        myList.remove(index);
+        System.out.println("\n" + "Remove object at position " + index);
+        System.out.print("Array list 2: " );
+        for (int i = 0; i < myList.size(); i++) {
+            System.out.print("\t" + myList.get(i));
+        }
+        System.out.println("\n" + "Array list size is: " + myList.size());
+
+        myList.clear();
+        System.out.println("\n" + "Clear array list");
+        System.out.print("Array list 2: " );
+        for (int i = 0; i < myList.size(); i++) {
+            System.out.print("\t" + myList.get(i));
+        }
+        System.out.println("\n" + "Array list size is: " + myList.size());
+  }
+}
